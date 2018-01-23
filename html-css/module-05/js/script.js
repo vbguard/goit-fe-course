77bd9920f6c5c5c7a9a921782d06344c71950a65
Viktor <admin@rizne.in.ua>
2018-01-23 21:25:11 +0200

    add image for module-06
    add coments for js module-05

--- html-css/module-05/js/script.js
+++ html-css/module-05/js/script.js
@@ -1,14 +1,28 @@
 var imgObj = document.getElementById('imageHolder'); 
 
 function nextImg() {
-  // for(i=0;i<n;i++)
-  // console.log(i);
-  var imageNum = 
+  // Беремо дані з атрибута src зображення
+  var imgsrc = document.getElementById('imageHolder').src;
+  // беремо з посилання номер картинки
+  var counter = parseInt(document.getElementById('imageHolder').src);
+  console.log(imgsrc);
+  // робимо перевірку на зміну зображення
+  if (counter < 4 ) { 
+    console.log(counter);
+    counter++;
+   }
+   else { counter = 1;}
+  console.log(counter);
   console.log(imgObj);
-  imgObj.setAttribute('src', 'img/image-2.jpg');
+
+  // виводимо зображення
+  imgObj.setAttribute('src', 'img/image-'+counter+'.jpg');
+  // return();
 }
 
 function prevImg() {
+  var counter = 4;
+  counter--;
   console.log(imgObj);
-  imgObj.setAttribute('src', 'img/image-1.jpg');
+  imgObj.setAttribute('src', 'img/image-'+counter+'.jpg');
 }
