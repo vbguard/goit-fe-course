1112e95f521b0e282593cae9d5b8349c3a02767a
Viktor Bulvarenko <viktor@bulvarenko.pw>
2018-05-11 20:49:11 +0300

    add js-module-04

--- js/module-04/js/index.js
+++ js/module-04/js/index.js
@@ -1,13 +1,5 @@
-/*
-  Создайте объект кассира, который получает список продуктов и деньги,
-  подсчитывает общую стоимость продуктов, и в зависимости от того
-  хватает денег или нет, уведомляет покупателя о результате.
-*/
+'use strict';
 
-/*
-  Есть заранее известная база данных товаров,
-  в формате "имя-товара":"цена за одну единицу"
-*/
 const products = {
   bread: 10,
   milk: 15,
@@ -19,63 +11,94 @@ const products = {
   candy: 25
 };
 
-/*
-  Необходимо создать функцию-конструктор Cashier для объектов кассира,
-  чтобы можно было создать сколько угодно кассиров.
-
-  Поля объекта кассира:
-    - name - строка, имя, передается при вызове конструктора
-
-    - products - объект база данных продуктов, передается при вызове конструктора
-
-    - totalPrice - число, общая стоимость покупок текущего покупателя, всегда начинается с 0
-
-    - customerMoney - число, сумма введенная пользователем в prompt при запросе денег, всегда начинается с 0
-
-    - changeAmount - число, сдача, всегда начинается с 0
-
-    - countTotalPrice(order) - метод, получает список покупок, считает общую сумму исходя из поля products
-
-    - getCustomerMoney() - метод, при вызове показывает prompt, в котором указана общая
-        сумма покупок из поля totalPrice. Ожидает ввода пользователя и записывает результат ввода в поле customerMoney.
-        Проверить что пользователь ввел число. Также customerMoney должна быть не меньше чем totalPrice.
-        Просит покупателя ввести сумму до тех пор, пока не будет введена корректная сумма - число,
-        равное или больше чем totalPrice. Если пользователь нажмет Cancel, то функция возвращает null.
-
-    - countChange() - метод, считает сдачу, разницу между общей суммой покупок и деньгами покупателя.
-
-    - reset() - метод, сбрасывает поля totalPrice, customerMoney и changeAmount в 0.
-
-    - serve(order) - метод, вызывается при обслуживании пользователя. Получает order - список покупок пользователя,
-        вызывает метод countTotalPrice для подсчета общей суммы, getCustomerMoney для запроса денег покупателя,
-        countChange для подсчета сдачи при успешном вводе пользователя. При успешном обслуживании возвращает
-        строку `Спасибо за покупку, ваша сдача ${сдача}`, при неудачном 'Очень жаль, что-то пошло не так, приходите еще'.
-        Вызывает метод reset при любом исходе обслуживания.
-*/
-
 function Cashier(name, products) {
-  // код
-  // не забывайте о this при обращении к свойствам и методам будущего объекта
-}
+  this.name = name;
+  this.products = products;
 
-/*
-  В метод serve будут передаваться объекты следующего формата. "имя-продукта": "количество-единиц"
-*/
-const order = {
-  bread: 2,
-  milk: 2,
-  apples: 1,
-  cheese: 1
+  let totalPrice = 0;
+  let changeAmount = 0;
+  let customerMoney = 0;
+
+  this.countTotalPrice = function () {
+    for (const key in products) {
+      if (products.hasOwnProperty(key)) {
+        const productsKeys = key;
+        const productsElement = products[key];
+        for (const key in order) {
+          if (order.hasOwnProperty(key)) {
+            const orderKeys = key;
+            const elementFromOrder = order[key];
+            if (productsKeys === orderKeys) {
+              const getSummOrderProducts = productsElement * elementFromOrder;
+              totalPrice += getSummOrderProducts;
+            }
+          }
+        }
+      }
+    }
+    return totalPrice;
+  };
+  this.getCustomerMoney = function () {
+    do {
+      customerMoney = prompt(`Загальна сумма ${totalPrice}, скільки грошей ви даєте?`, '');
+      const isInRange = totalPrice <= Number(customerMoney);
+      const isNumber = !Number.isNaN(Number(customerMoney));
+      let isValidInput = isInRange && isNumber;
+      if (customerMoney === null) {
+        return null
+      };
+      if (isValidInput) {
+        customerMoney = Number(customerMoney);
+        break;
+      }
+    } while (true);
+
+    return customerMoney;
+  };
+  this.countChange = function () {
+    console.log(customerMoney);
+    console.log(totalPrice);
+    const changeCustomerShop = customerMoney - totalPrice;
+    return changeAmount = changeCustomerShop;
+  };
+  this.reset = function () {
+    if (totalPrice > 0) {
+      totalPrice = 0;
+    };
+    if (customerMoney > 0) {
+      customerMoney = 0;
+    };
+    if (changeAmount > 0) {
+      changeAmount = 0
+    };
+  };
+  this.serve = function (order) {
+    this.countTotalPrice(order);
+    if (this.getCustomerMoney() === null) {
+      this.reset();
+      return alert('Очень жаль, что-то пошло не так, приходите еще');
+    }
+    this.countChange();
+    alert(`Спасибо за покупку, ваша сдача ${changeAmount}`);
+    this.reset();
+  };
 };
 
-/*
-  Ниже приведен пример использования.
-*/
+const order = {
+  bread: 2,
+  milk: 4,
+  apples: 1,
+  cheese: 3
+};
+
+const order2 = {
+  bread: 2,
+  milk: 4,
+  apples: 1,
+  cheese: 3
+};
 
 const cashier = new Cashier('Mango', products);
-
 cashier.serve(order);
-/*
-  При вызове cashier.serve, countTotalPrice посчитает общую сумму равную 20+30+20+60, итого 130
-  Далее выполнение идет по вышеописанному алгоритму.
-*/
+const cashier2 = new Cashier('Tago', products);
+cashier2.serve(order);
