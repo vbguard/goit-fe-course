82a2208dd55faf0589f94b829d68729583753cb5
Viktor Bulvarenko <viktor@bulvarenko.pw>
2018-05-14 12:59:21 +0300

    add third solution method countTotalPrice() in js-module-04

--- js/module-04/js/index.js
+++ js/module-04/js/index.js
@@ -8,7 +8,7 @@ const products = {
   pork: 80,
   cheese: 60,
   tea: 20,
-  candy: 25
+  candy: 25,
 };
 
 function Cashier(name, products) {
@@ -19,7 +19,7 @@ function Cashier(name, products) {
   this.changeAmount = 0;
   this.customerMoney = 0;
 
-  this.countTotalPrice = function (order) {
+  this.countTotalPrice = function(order) {
     this.order = order;
     // for (const key in this.products) {
     //   if (this.products.hasOwnProperty(key)) {
@@ -37,36 +37,50 @@ function Cashier(name, products) {
     //     }
     //   }
     // }
-    for (const key in this.order) {
-      if (this.order.hasOwnProperty(key)) {
-        this.totalPrice += this.order[key] * this.products[key];
-      }
+    //========================
+    // for (const key in this.order) {
+    //   if (this.order.hasOwnProperty(key)) {
+    //     this.totalPrice += this.order[key] * this.products[key];
+    //   }
+    // }
+    //========================
+    const keys = Object.keys(order);
+
+    console.log('keys const: ', keys);
+
+    for (let key of keys) {
+      this.totalPrice += this.order[key] * this.products[key];
     }
   };
-  this.getCustomerMoney = function () {
+  this.getCustomerMoney = function() {
     do {
-      this.customerMoney = prompt(`Вас вітає касир ${this.name}. Загальна сумма ${this.totalPrice}, скільки грошей ви даєте?`, '');
+      this.customerMoney = prompt(
+        `Вас вітає касир ${this.name}. Загальна сумма ${
+          this.totalPrice
+        }, скільки грошей ви даєте?`,
+        '',
+      );
       const isInRange = this.totalPrice <= Number(this.customerMoney);
       const isNumber = !Number.isNaN(Number(this.customerMoney));
       let isValidInput = isInRange && isNumber;
       if (this.customerMoney === null) {
-        return null
-      };
+        return null;
+      }
       if (isValidInput) {
         this.customerMoney = Number(this.customerMoney);
         break;
       }
     } while (true);
   };
-  this.countChange = function () {
+  this.countChange = function() {
     this.changeAmount = this.customerMoney - this.totalPrice;
   };
-  this.reset = function () {
+  this.reset = function() {
     this.totalPrice = 0;
     this.customerMoney = 0;
     this.changeAmount = 0;
   };
-  this.serve = function (order) {
+  this.serve = function(order) {
     this.order = order;
     this.countTotalPrice(this.order);
     this.getCustomerMoney();
@@ -79,19 +93,19 @@ function Cashier(name, products) {
     alert(`Спасибо за покупку, ваша сдача ${this.changeAmount}`);
     this.reset();
   };
-};
+}
 
 const order = {
   bread: 2,
   milk: 2,
   apples: 1,
-  cheese: 1
+  cheese: 1,
 };
 
 const order2 = {
   bread: 1,
   milk: 1,
-  apples: 1
+  apples: 1,
 };
 
 const cashier = new Cashier('Mango', products);
